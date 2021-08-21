d621feb3d96ee516b7e226d32c011836eef3bac5
prince-deb <78799922+prince-deb@users.noreply.github.com>
2021-08-21 22:29:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightwristX=0;
+lrftwristX=0;
 function setup(){
-    canvas=createCanvas(300,300)
-    canvas.center()
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,550);
+     canvas.position(560,150);
+     poseNet=ml5.poseNet(video,modelLoaded);
+     poseNet.on('pose',gotPoses);
 }
 function draw(){
-
+    background('#ff9d00');
+    textSize(difference);
+    fill('#FFE787');
+    text('prince',50,400);
+}
+function modelLoaded(){
+console.log("modelis loaded");
+}
+function gotPoses(results)
+{
+    if (results.length>0)
+    {
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX="+noseX+"noseY"+noseY);
+        leftwristX=results[0].pose.leftWrist.x;
+        rightwristX=results[0].pose.rightWrist.x;   
+        difference=leftwristX-rightwristX;
+        console.log("leftwristX"+leftwristX+"rightwristX"+rightwristX+"difference"+difference);
+    }
 }
